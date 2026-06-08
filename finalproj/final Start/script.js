247c9ea061b31d91a0323d5133e89a9e118d868e
Dansowaa A <dansowaaa@nycstudents.net>
2026-06-08 12:18:42 -0400

    audio

--- finalproj/final Start/script.js
+++ finalproj/final Start/script.js
@@ -126,6 +126,12 @@ const lyricsDatabase = {
 [01:19.00] 'Til the day that I die
 [01:23.00] 'Til the light leaves my eyes
     `,
+  "i-hope-this-doesnt-find-you": `
+[00:00.00] (Intro)
+[00:05.00] This is a sample excerpt for the audiobook.
+[00:30.00] Chapter 1 starts: The narrator speaks...
+[01:00.00] Chapter 1 continues: A short passage.
+    `,
 };
 
 // Fixed time parser helper
@@ -234,6 +240,9 @@ if (lyricsAudioBtn) {
     if (mainHeading && mainHeading.textContent.includes("Billie Jean")) {
       audioSrc = "Michael Jackson - Billie Jean (Official Video).mp3"; 
       currentSong = "billie-jean";
+    } else if (mainHeading && mainHeading.textContent.includes("I Hope This Doesn't Find You")) {
+      audioSrc = "I Hope This Doesn't Find You.mp3";
+      currentSong = "i-hope-this-doesnt-find-you";
     } else if (mainHeading && mainHeading.textContent.includes("Taylor Swift")) {
       audioSrc = "taylor_stift_mo3.mp3"; 
       currentSong = "taylor-swift-mo3";
